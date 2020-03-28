0e571a7fdb3e6b8cc845de2da774deb631424e7a
Mohamed Arsad <mohamedarsad@Mohameds-Air.local>
2020-03-28 14:13:23 +0530

    Add Basic Programs

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+  int a,b;
+  printf("Enter the number 1: ")
+  scanf("%d",&a);
+  printf("Enter the number 2: ")
+  scanf("%d",&b);
+  printf("Sum of %d and %d is %d",a,b,a+b);
+}
