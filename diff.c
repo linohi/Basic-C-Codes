9675d0c7e3746235f758df355169403b291932fc
Mohamed Arsad <mohamedarsad@Mohameds-Air.local>
2020-03-28 14:18:00 +0530

    Add Difference Program

---
+++ diff.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	printf("Enter the number 1: ")
+	scanf("%d",&a);
+	printf("Enter the number 2: ")
+	scanf("%d",&b);
+	printf("Difference between %d and %d is %d",a,b,a-b);
+}
